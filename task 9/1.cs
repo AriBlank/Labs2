11104f0f7fc819c86629ebb3d4d9f530fe5f17ab
AriBlank <arusa668@gmail.com>
2026-05-21 04:15:31 +0300

    add synchronous function decorator with configurable log level

--- task 9/1.cs
+++ task 9/1.cs
@@ -12,4 +12,28 @@ public enum LogLevel
 public static class Log
 {
     public static Action<string> WriteLine { get; set; } = Console.WriteLine;
+    public static Func<T> Decorate<T>(Func<T> func, LogLevel level)
+    {
+        return () =>
+        {
+            var sw = Stopwatch.StartNew();
+            try
+            {
+                if (level != LogLevel.Error)
+                    WriteLine($"[{DateTime.Now:HH:mm:ss.fff}] [{level}] enter: {func.Method.Name}");
+
+                var result = func();
+
+                if (level != LogLevel.Error)
+                    WriteLine($"[{DateTime.Now:HH:mm:ss.fff}] [{level}] exit: {result} [{sw.ElapsedMilliseconds}ms]");
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                WriteLine($"[{DateTime.Now:HH:mm:ss.fff}] [ERROR] {func.Method.Name}: {ex.Message}");
+                throw;
+            }
+        };
+    }
 }
