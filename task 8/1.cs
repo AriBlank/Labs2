0570c12a2110240a3e11ca64589567f423a754a6
AriBlank <arusa668@gmail.com>
2026-05-21 03:17:25 +0300

    inject api key into request headers

--- task 8/1.cs
+++ task 8/1.cs
@@ -5,10 +5,19 @@ using System.Threading.Tasks;
 
 public class AuthenticationProxy : IDisposable
 {
+    private readonly string _apiKey;
+    private readonly string _headerName;
     private readonly HttpClient _httpClient;
     public AuthenticationProxy(string baseAddress, string apiKey, string headerName = "X-API-Key")
     {
         _httpClient = new HttpClient { BaseAddress = new Uri(baseAddress) };
+        _apiKey = apiKey;
+        _headerName = headerName;
+    }
+    public async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken = default)
+    {
+        request.Headers.Add(_headerName, _apiKey);
+        return await _httpClient.SendAsync(request, cancellationToken);
     }
     public void Dispose()
     {
